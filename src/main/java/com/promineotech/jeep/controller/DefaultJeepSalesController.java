8443cbb012c9e9d65f5268390eb968a3ae5cb309
gyarbrough <yarbroughgj@gmail.com>
2022-10-22 13:37:19 -0600

    sixth commit

--- src/main/java/com/promineotech/jeep/controller/DefaultJeepSalesController.java
+++ src/main/java/com/promineotech/jeep/controller/DefaultJeepSalesController.java
@@ -10,7 +10,7 @@ import lombok.extern.slf4j.Slf4j;
 
 @RestController
 @Slf4j
-public class BasicJeepSalesController implements JeepSalesController {
+public class DefaultJeepSalesController implements JeepSalesController {
   
   @Autowired
   private JeepSalesService jeepSalesService;
--- src/test/java/com/promineotech/jeep/controller/support/BaseTest.java
+++ src/test/java/com/promineotech/jeep/controller/support/BaseTest.java
@@ -2,7 +2,7 @@ package com.promineotech.jeep.controller.support;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.web.client.TestRestTemplate;
-import org.springframework.boot.web.server.LocalServerPort;
+import org.springframework.boot.test.web.server.LocalServerPort;
 import lombok.Getter;
 
 public class BaseTest {
@@ -13,7 +13,11 @@ public class BaseTest {
   @Getter
   private TestRestTemplate restTemplate;
 
-  protected String getBaseUri() {
+  protected String getBaseUriForJeeps() {
     return String.format("http://localhost:%d/jeeps", serverPort);
+  }
+  
+  protected String getBaseUriForOrders() {
+    return String.format("http://localhost:%d/orders", serverPort);
   }
 }
